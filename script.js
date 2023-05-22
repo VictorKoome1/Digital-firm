cd80fb1bcc0c1f5aeda52316730f7b005f54eb3c
Victor Koome <victorkoome485@gmail.com>
2023-05-22 14:31:41 +0300

    finished project files

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const navLinks = document.getElementById("navLinks");
 
-        function showMenu() {
-            navLinks.style.right = "0";
-        }
 
-        function hideMenu() {
-            navLinks.style.right = "-200px";
-        }
+sideEl = document.getElementById("side-panel")
+bannerEl = document.getElementById("banner")
+bodyEl = document.getElementById("body")
+timesEl = document.getElementById("timesx")
+universalEl = document.querySelector("*")
+
+
+
+function clicked() {
+    sideEl.style.left = "0px";
+    sideEl.style.opacity = "1"
+    // bodyEl.style.opacity ="0.9"
+    // bodyEl.style.collapse = "40px"
+    bannerEl.style.opacity = "0.8";
+    universalEl.style.overflow = "hidden"
+}
+
+function collapse() {
+    sideEl.style.left = "-250px";
+    bodyEl.style.opacity ="1";
+    universalEl.style.overflow = "scroll"
+}
+
